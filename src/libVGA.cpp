5d5e1e02e69de57cbae139c45c68ceb9406c5a64
Jeremy Utt <jeremyu2022@gmail.com>
2025-02-05 14:22:29 -0700

    removed redundant function

--- src/libVGA.cpp
+++ src/libVGA.cpp
@@ -5,12 +5,6 @@
 #include <printf.hpp>
 #include <tests.hpp>
 
-void outportb(unsigned short port, unsigned char val) {
-    outb(port, val);
-}
-unsigned char inportb(unsigned short port) {
-    return inb(port);
-}
 void _vmemwr(unsigned seg, unsigned dst_off, unsigned char *src, unsigned count) {
     // Calculate the linear address
     uint32_t address = (seg << 4) + dst_off;
@@ -54,7 +48,7 @@ To do:
 // #include <string.h> /* movedata(), memcpy() */
 // #include <stdio.h> /* printf() */
 // #include <conio.h> /* getch() */
-// #include <dos.h> /* FP_SEG(), FP_OFF(), inportb(), outportb() */
+// #include <dos.h> /* FP_SEG(), FP_OFF(), inb(), outb() */
 #define VGA_AC_INDEX 0x3C0
 #define VGA_AC_WRITE 0x3C0
 #define VGA_AC_READ 0x3C1
@@ -1428,80 +1422,80 @@ void read_regs(unsigned char *regs) {
     unsigned i;
 
     /* read MISCELLANEOUS reg */
-    *regs = inportb(VGA_MISC_READ);
+    *regs = inb(VGA_MISC_READ);
     regs++;
     /* read SEQUENCER regs */
     for (i = 0; i < VGA_NUM_SEQ_REGS; i++) {
-        outportb(VGA_SEQ_INDEX, i);
-        *regs = inportb(VGA_SEQ_DATA);
+        outb(VGA_SEQ_INDEX, i);
+        *regs = inb(VGA_SEQ_DATA);
         regs++;
     }
     /* read CRTC regs */
     for (i = 0; i < VGA_NUM_CRTC_REGS; i++) {
-        outportb(VGA_CRTC_INDEX, i);
-        *regs = inportb(VGA_CRTC_DATA);
+        outb(VGA_CRTC_INDEX, i);
+        *regs = inb(VGA_CRTC_DATA);
         regs++;
     }
     /* read GRAPHICS CONTROLLER regs */
     for (i = 0; i < VGA_NUM_GC_REGS; i++) {
-        outportb(VGA_GC_INDEX, i);
-        *regs = inportb(VGA_GC_DATA);
+        outb(VGA_GC_INDEX, i);
+        *regs = inb(VGA_GC_DATA);
         regs++;
     }
     /* read ATTRIBUTE CONTROLLER regs */
     for (i = 0; i < VGA_NUM_AC_REGS; i++) {
-        (void)inportb(VGA_INSTAT_READ);
-        outportb(VGA_AC_INDEX, i);
-        *regs = inportb(VGA_AC_READ);
+        (void)inb(VGA_INSTAT_READ);
+        outb(VGA_AC_INDEX, i);
+        *regs = inb(VGA_AC_READ);
         regs++;
     }
     /* lock 16-color palette and unblank display */
-    (void)inportb(VGA_INSTAT_READ);
-    outportb(VGA_AC_INDEX, 0x20);
+    (void)inb(VGA_INSTAT_READ);
+    outb(VGA_AC_INDEX, 0x20);
 }
 
 void write_regs(unsigned char *regs) {
     unsigned i;
 
     /* write MISCELLANEOUS reg */
-    outportb(VGA_MISC_WRITE, *regs);
+    outb(VGA_MISC_WRITE, *regs);
     regs++;
     /* write SEQUENCER regs */
     for (i = 0; i < VGA_NUM_SEQ_REGS; i++) {
-        outportb(VGA_SEQ_INDEX, i);
-        outportb(VGA_SEQ_DATA, *regs);
+        outb(VGA_SEQ_INDEX, i);
+        outb(VGA_SEQ_DATA, *regs);
         regs++;
     }
     /* unlock CRTC registers */
-    outportb(VGA_CRTC_INDEX, 0x03);
-    outportb(VGA_CRTC_DATA, inportb(VGA_CRTC_DATA) | 0x80);
-    outportb(VGA_CRTC_INDEX, 0x11);
-    outportb(VGA_CRTC_DATA, inportb(VGA_CRTC_DATA) & ~0x80);
+    outb(VGA_CRTC_INDEX, 0x03);
+    outb(VGA_CRTC_DATA, inb(VGA_CRTC_DATA) | 0x80);
+    outb(VGA_CRTC_INDEX, 0x11);
+    outb(VGA_CRTC_DATA, inb(VGA_CRTC_DATA) & ~0x80);
     /* make sure they remain unlocked */
     regs[0x03] |= 0x80;
     regs[0x11] &= ~0x80;
     /* write CRTC regs */
     for (i = 0; i < VGA_NUM_CRTC_REGS; i++) {
-        outportb(VGA_CRTC_INDEX, i);
-        outportb(VGA_CRTC_DATA, *regs);
+        outb(VGA_CRTC_INDEX, i);
+        outb(VGA_CRTC_DATA, *regs);
         regs++;
     }
     /* write GRAPHICS CONTROLLER regs */
     for (i = 0; i < VGA_NUM_GC_REGS; i++) {
-        outportb(VGA_GC_INDEX, i);
-        outportb(VGA_GC_DATA, *regs);
+        outb(VGA_GC_INDEX, i);
+        outb(VGA_GC_DATA, *regs);
         regs++;
     }
     /* write ATTRIBUTE CONTROLLER regs */
     for (i = 0; i < VGA_NUM_AC_REGS; i++) {
-        (void)inportb(VGA_INSTAT_READ);
-        outportb(VGA_AC_INDEX, i);
-        outportb(VGA_AC_WRITE, *regs);
+        (void)inb(VGA_INSTAT_READ);
+        outb(VGA_AC_INDEX, i);
+        outb(VGA_AC_WRITE, *regs);
         regs++;
     }
     /* lock 16-color palette and unblank display */
-    (void)inportb(VGA_INSTAT_READ);
-    outportb(VGA_AC_INDEX, 0x20);
+    (void)inb(VGA_INSTAT_READ);
+    outb(VGA_AC_INDEX, 0x20);
 }
 
 static void set_plane(unsigned p) {
@@ -1510,11 +1504,11 @@ static void set_plane(unsigned p) {
     p &= 3;
     pmask = 1 << p;
     /* set read plane */
-    outportb(VGA_GC_INDEX, 4);
-    outportb(VGA_GC_DATA, p);
+    outb(VGA_GC_INDEX, 4);
+    outb(VGA_GC_DATA, p);
     /* set write plane */
-    outportb(VGA_SEQ_INDEX, 2);
-    outportb(VGA_SEQ_DATA, pmask);
+    outb(VGA_SEQ_INDEX, 2);
+    outb(VGA_SEQ_DATA, pmask);
 }
 
 /*****************************************************************************
@@ -1524,8 +1518,8 @@ depending on bits in GC 6
 static unsigned get_fb_seg(void) {
     unsigned seg;
 
-    outportb(VGA_GC_INDEX, 6);
-    seg = inportb(VGA_GC_DATA);
+    outb(VGA_GC_INDEX, 6);
+    seg = inb(VGA_GC_DATA);
     seg >>= 2;
     seg &= 3;
     switch (seg) {
@@ -1564,27 +1558,27 @@ static void write_font(unsigned char *buf, unsigned font_height) {
 
     /* save registers
     set_plane() modifies GC 4 and SEQ 2, so save them as well */
-    outportb(VGA_SEQ_INDEX, 2);
-    seq2 = inportb(VGA_SEQ_DATA);
+    outb(VGA_SEQ_INDEX, 2);
+    seq2 = inb(VGA_SEQ_DATA);
 
-    outportb(VGA_SEQ_INDEX, 4);
-    seq4 = inportb(VGA_SEQ_DATA);
+    outb(VGA_SEQ_INDEX, 4);
+    seq4 = inb(VGA_SEQ_DATA);
     /* turn off even-odd addressing (set flat addressing)
     assume: chain-4 addressing already off */
-    outportb(VGA_SEQ_DATA, seq4 | 0x04);
+    outb(VGA_SEQ_DATA, seq4 | 0x04);
 
-    outportb(VGA_GC_INDEX, 4);
-    gc4 = inportb(VGA_GC_DATA);
+    outb(VGA_GC_INDEX, 4);
+    gc4 = inb(VGA_GC_DATA);
 
-    outportb(VGA_GC_INDEX, 5);
-    gc5 = inportb(VGA_GC_DATA);
+    outb(VGA_GC_INDEX, 5);
+    gc5 = inb(VGA_GC_DATA);
     /* turn off even-odd addressing */
-    outportb(VGA_GC_DATA, gc5 & ~0x10);
+    outb(VGA_GC_DATA, gc5 & ~0x10);
 
-    outportb(VGA_GC_INDEX, 6);
-    gc6 = inportb(VGA_GC_DATA);
+    outb(VGA_GC_INDEX, 6);
+    gc6 = inb(VGA_GC_DATA);
     /* turn off even-odd addressing */
-    outportb(VGA_GC_DATA, gc6 & ~0x02);
+    outb(VGA_GC_DATA, gc6 & ~0x02);
     /* write font to plane P4 */
     set_plane(2);
     /* write font 0 */
@@ -1601,16 +1595,16 @@ static void write_font(unsigned char *buf, unsigned font_height) {
 	}
 #endif
     /* restore registers */
-    outportb(VGA_SEQ_INDEX, 2);
-    outportb(VGA_SEQ_DATA, seq2);
-    outportb(VGA_SEQ_INDEX, 4);
-    outportb(VGA_SEQ_DATA, seq4);
-    outportb(VGA_GC_INDEX, 4);
-    outportb(VGA_GC_DATA, gc4);
-    outportb(VGA_GC_INDEX, 5);
-    outportb(VGA_GC_DATA, gc5);
-    outportb(VGA_GC_INDEX, 6);
-    outportb(VGA_GC_DATA, gc6);
+    outb(VGA_SEQ_INDEX, 2);
+    outb(VGA_SEQ_DATA, seq2);
+    outb(VGA_SEQ_INDEX, 4);
+    outb(VGA_SEQ_DATA, seq4);
+    outb(VGA_GC_INDEX, 4);
+    outb(VGA_GC_DATA, gc4);
+    outb(VGA_GC_INDEX, 5);
+    outb(VGA_GC_DATA, gc5);
+    outb(VGA_GC_INDEX, 6);
+    outb(VGA_GC_DATA, gc6);
 }
 
 /*****************************************************************************
